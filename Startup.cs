96a61f535e27e355b3ee7ea6f646c3766f4e14d4
Gabrielle Cezanne <gabriellecezanne@gmail.com>
2021-03-10 20:10:53 -0600

    Merging the updates

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace HealthCheck
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
+
         {
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
@@ -26,6 +27,10 @@ namespace HealthCheck
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddHealthChecks()
+                .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
+                .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
+                .AddCheck("ICMP_03", new ICMPHealthCheck("www.does-not-exist.com", 100));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +47,7 @@ namespace HealthCheck
                 app.UseHsts();
             }
 
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles(new StaticFileOptions()
             {
@@ -65,11 +71,11 @@ namespace HealthCheck
 
             app.UseRouting();
 
+            app.UseHealthChecks("/hc", new CustomHealthCheckOptions());
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
